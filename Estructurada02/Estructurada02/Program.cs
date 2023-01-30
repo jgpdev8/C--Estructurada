192ccaaf13c4070a659b649fe05a1c7a50a43f93
jgpdev8 <123153789+jgpdev8@users.noreply.github.com>
2023-01-30 17:00:22 +0100

    Add files via upload

---
+++ Estructurada02/Estructurada02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Estructurada02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anno;
+            Console.WriteLine("Dime el año para saber si es bisiesto o no");
+            anno = int.Parse(Console.ReadLine());
+            if (anno % 4 == 0)
+            {
+                Console.WriteLine("El año "+anno+" es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año "+anno+" no es bisiesto");
+            }
+        }
+    }
+}
