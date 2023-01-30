6ad7a4bb2a3b7494df98bf8a3155679fd3cb118e
jgpdev8 <123153789+jgpdev8@users.noreply.github.com>
2023-01-30 17:06:13 +0100

    Add files via upload

---
+++ Estructurada03/Estructurada03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Estructurada03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dinero, dinero2;
+            string moneda;
+            Console.WriteLine("Dime la cantidad de dinero");
+            dinero = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dime a que moneda quieres pasar peseta o euro");
+            moneda = Console.ReadLine();
+            if (moneda == "euro")
+            {
+                dinero2 = dinero / 166.386;
+                Console.WriteLine(dinero2);
+            }
+            else
+            {
+                if (moneda == "peseta")
+                {
+                    dinero2 = dinero * 166.386;
+                    Console.WriteLine(dinero2);
+                }
+                else
+                {
+                    Console.WriteLine("tipo de moneda erróneo");
+                }
+            }
+        }
+    }
+}
