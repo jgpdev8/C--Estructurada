4cfa9721d59a02e78fd79bdbaef0b412cfa21d54
jgpdev8 <123153789+jgpdev8@users.noreply.github.com>
2023-01-30 16:59:51 +0100

    Add files via upload

---
+++ Estructurada01/Estructurada01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Estructurada01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Dime el primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dime el segundo numero");
+            b = int.Parse(Console.ReadLine());
+
+            if (a >= b)
+            {
+                Console.WriteLine(a+"-"+b+"="+(a-b));
+            }
+            else
+            {
+                Console.WriteLine(b+"-"+a+"="+(b-a));
+            }
+        }
+    }
+}
